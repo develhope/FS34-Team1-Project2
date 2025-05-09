c3691a93910dd00b72a29b7c07ca05126339aa2f
DeBugMarino <159265842+DeBugMarino@users.noreply.github.com>
2025-05-09 19:48:03 +0200

    installato libreria + aggiunto grafico

--- project2/src/components/Dashboard.jsx
+++ project2/src/components/Dashboard.jsx
@@ -1,5 +1,6 @@
-import { Link } from "react-router-dom";
+import BarChart from "./BarChart";
 import Navbar from "./Navbar";
+import PieChart from "./PieChart";
 
 export default function Dashboard() {
   return (
@@ -190,25 +191,10 @@ export default function Dashboard() {
                       <span className="text-2xl sm:text-3xl leading-none font-bold text-gray-900">
                         $45,385
                       </span>
-                      <h3 className="text-base font-normal text-gray-500">
-                        Sales this week
-                      </h3>
+                      <PieChart></PieChart>
                     </div>
-                    <div className="flex items-center justify-end flex-1 text-green-500 text-base font-bold">
-                      12.5%
-                      <svg
-                        className="w-5 h-5"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z"
-                          clipRule="evenodd"
-                        ></path>
-                      </svg>
-                    </div>
+
+                    <BarChart></BarChart>
                   </div>
                   <div id="main-chart"></div>
                 </div>
--- project2/src/components/Registrazione.jsx
+++ project2/src/components/Registrazione.jsx
@@ -5,12 +5,11 @@ import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
 export default function Registrazione() {
   const { registrazione, error, validate, users } = useAuth();
-<<<<<<< HEAD
-=======
+
   const ids = users.filter((user) => user.id);
-  const idMax = users.reduce((max, user) => (user.id > max ? user.id : max), 0);
+
   console.log("idmax" + idMax);
->>>>>>> 34796beebada6e981c0be96328aa5ad7bce9ea17
+
   const [messaggioErrore, setMessaggioErrore] = useState(null);
   const navigate = useNavigate();
 
@@ -43,11 +42,8 @@ export default function Registrazione() {
       alert(validationError);
       return;
     }
-<<<<<<< HEAD
-    const result = registrazione(user);
-=======
+
     const result = await registrazione(user);
->>>>>>> 34796beebada6e981c0be96328aa5ad7bce9ea17
 
     if (!result.esito) {
       setMessaggioErrore(result.messaggio);
@@ -195,7 +191,6 @@ export default function Registrazione() {
         >
           <button
             onClick={(e) => setMessaggioErrore(null)}
-<<<<<<< HEAD
             className="text-gray-500 hover:text-gray-700 focus:outline-none"
           >
             <svg
@@ -212,12 +207,6 @@ export default function Registrazione() {
                 d="M6 18L18 6M6 6l12 12"
               />
             </svg>
-=======
-            className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
-          >
-            {" "}
-            ✖
->>>>>>> 34796beebada6e981c0be96328aa5ad7bce9ea17
           </button>
           <p className=" py-5 px-10 font-medium text-black text-lg">
             {messaggioErrore}
