f2fba97220f080a5f2a2d49a886d1a0da7faec8e
DeBugMarino <159265842+DeBugMarino@users.noreply.github.com>
2025-05-16 14:31:22 +0200

    server

--- project2/src/backend/server.js
+++ project2/src/backend/server.js
@@ -1,54 +1,35 @@
 import express from "express";
 import cors from "cors";
-import { utenti } from "./utenti.js";
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-app.post("/utente", (req, res) => {
-  const { id, nome, cognome, email, eta, password } = req.body;
-  const userExist = utenti.find(
-    (user) => user.email.toLowerCase() === email.toLowerCase()
-  );
-  if (userExist) {
-    return res.status(404).json({ message: "utente già registrato" });
+const utenti = [];
+
+app.get("/", (req, res) => {
+  if (utenti) {
+    res.json(utenti);
+  } else if (utenti.length) {
+    res.status(404).send("errore utenti non trovati");
   } else {
-    const newUser = {
-      id: id,
-      nome: nome,
-      cognome: cognome,
-      email: email,
-      eta: eta,
-      password: password,
-    };
-    utenti.push(newUser);
-    return res.status(201).json({ message: "utente registrato con successo" });
+    res.status(404).send("errore la pagina non è stata trovata");
   }
+});
+
+app.post("/registrazione", (req, res) => {
+  const { nome, cognome, email, eta, password } = req.body;
+  utenti.push({ nome, cognome, email, eta, cellulare, password });
+  res.status(201).json({ message: "Utente registrato con successo" });
 });
 
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
-  if (email && password) {
-    const userExist = utenti.find(
-      (utente) =>
-        utente.email.toLowerCase() === email.toLowerCase() &&
-        utente.password === password
-    );
-    if (userExist) {
-      return res
-        .status(200)
-        .json({ message: "login effettuato con successo", user: userExist });
-    } else {
-      return res.status(400).json({ message: "credenziali errate" });
-    }
-  } else {
-    return res.status(404).json({ message: "inserisci email e password" });
-  }
+  res.status(200).json({ message: "Login ricevuto", email, password });
 });
 
 app.listen(PORT, () => {
-  console.log(`avviato il server su http://localhost:${PORT} `);
+  console.log(`Server avviato su http://localhost:${PORT}`);
 });
