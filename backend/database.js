6017213060a8d59695fa5f4e932c62c614921762
Martina Costantino <martinacostantino@yahoo.it>
2025-05-26 17:38:10 +0200

    m

--- backend/database.js
+++ backend/database.js
@@ -1,24 +1,25 @@
-import mysql from "mysql2/promise";
+import mysql from 'mysql2/promise';
+
 
 const db = dataBase({
   host: "localhost",
   port: 80,
-  database: celestique,
+  database: celestique
 });
 
 db.none(
-  `CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY, 
-  nome VARCHAR NOT NULL, 
-  cognome VARCHAR NOT NULL, 
-  email VARCHAR NOT NULL UNIQUE
-  eta INT NOT NULL , 
-  password VARCHAR NOT NULL,
-  cellulare VARCHAR NOT NULL,
-  )`
+` CREATE TABLE IF NOT EXISTS users (
+id SERIAL PRIMARY KEY, 
+nome VARCHAR NOT NULL, 
+cognome VARCHAR NOT NULL, 
+email VARCHAR NOT NULL UNIQUE
+eta INT NOT NULL , 
+password VARCHAR NOT NULL,
+cellulare VARCHAR NOT NULL,
+)`
 )
   .then(() => console.log("Tabella creata correttamente"))
-
+  
   .catch((error) =>
     console.error("Errore durante la creazione della tabella", error)
   );
