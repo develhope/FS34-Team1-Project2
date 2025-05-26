aba457889dde6188f9e0e0a1ea73d46a8e239509
Danichiesa03 <edoardochiesa3@gmail.com>
2025-05-26 18:29:28 +0200

    d

--- backend/database.js
+++ backend/database.js
@@ -1,14 +1,15 @@
-import mysql from 'mysql2/promise';
+import pgPromise from "pg-promise";
 
+const dataBase = pgPromise();
 
 const db = dataBase({
   host: "localhost",
-  port: 80,
-  database: celestique
+  port: 5432,
+  database: "celestique",
 });
 
 db.none(
-` CREATE TABLE IF NOT EXISTS users (
+  ` CREATE TABLE IF NOT EXISTS users (
 id SERIAL PRIMARY KEY, 
 nome VARCHAR NOT NULL, 
 cognome VARCHAR NOT NULL, 
@@ -19,7 +20,7 @@ cellulare VARCHAR NOT NULL,
 )`
 )
   .then(() => console.log("Tabella creata correttamente"))
-  
+
   .catch((error) =>
     console.error("Errore durante la creazione della tabella", error)
   );
